319620bde1839b0c3274ff5abf3be454a096beeb
pradeep <singhpardeep585@gmail.com>
2023-04-04 18:38:00 +0530

    added global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        dark_primary: "var(--dark-primary)",
+        dark_secondary: "var(--dark-secondary)",
+        dark_accent: "var(--dark-accent)",
+        light: "var(--light)",
+        dark: "var(--dark)",
+      },
     },
   },
   plugins: [],
-}
-
+};
