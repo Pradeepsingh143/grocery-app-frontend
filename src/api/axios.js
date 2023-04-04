b0dc9a9d04e0816547a537be809a9a3ac6b3774d
pradeep <singhpardeep585@gmail.com>
2023-04-04 18:38:31 +0530

    added search functionality in navbar

---
+++ src/api/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// const BASE_URL = 'https://ecommerceapp-production-3897.up.railway.app';
+const BASE_URL = 'http://localhost:4000';
+
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from "react";
 import { BsFillMoonFill, BsFillSunFill, BsWindowDesktop } from "react-icons/bs";
+import axios from "../../api/axios";
 
 const Navbar = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "system"
   );
   const element = document.documentElement;
   const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
-  console.log(darkQuery);
   const themeOptions = [
     {
       icon: <BsFillSunFill />,
@@ -63,33 +68,93 @@ const Navbar = (props) => {
       }
     }
   });
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get("/api/collection/get");
+      setCategory(data.collection);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClick = () => {
+    if (category.length === 0) {
+      fetchCategories();
+    }
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`/api/product/search/${searchTerm}/${selectedCategory}`);
+      setSearchResults(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   return (
-    <header className="bg-slate-900 dark:bg-slate-800">
-      <div className="container mx-auto items-center grid grid-cols-3">
-        <div className="logo justify-self-start">webiste logo</div>
-        <nav className="navigation justify-self-end">
-          <ul className="list-none flex space-x-10">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-        <div className="flex space-x-3 justify-self-end">
-          {themeOptions.map((opt) => (
-            <button
-              key={opt.text}
-              onClick={() => setTheme(opt.text)}
-              className={`w-5 h-5 rounded-full text-xl leading-8 ${
-                theme === opt.text && "text-amber-600"
-              } transition ease-in duration-200`}
+    <header className="text-xs text-dark dark:text-light">
+      <div className="header-top-wrapper border-b">
+        <div className="container mx-auto px-4 py-3 grid justify-center text-center sm:grid-cols-2 sm:gap-2 sm:py-4">
+          <div className="sm:justify-self-start">
+            <h3>
+              Working Time: <strong>Mon-Sat:8:00-21:00</strong>
+            </h3>
+          </div>
+          <div className="sm:justify-self-end">
+            <ul className="hidden sm:flex list-none sm:gap-4 sm:leading-4 sm:opacity-80">
+              <li className="border-r pr-2 border-dark">My Account</li>
+              <li className="border-r pr-2 border-dark">About Us</li>
+              <li className="border-r pr-2 border-dark">Contact</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="header-search-area">
+        <div className="container mx-auto">
+          <div className="logo">website logo</div>
+          <div className="search-bar">
+            <select
+              value={selectedCategory}
+              onClick={handleClick}
+              onChange={(e) => setSelectedCategory(e.target.value)}
             >
-              {opt.icon}
-            </button>
-          ))}
+              <option value="">All Categories</option>
+              {category.map((item) => (
+                <option key={item._id} value={item.name}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              placeholder="Search Product..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button
+            disabled = {searchTerm && selectedCategory ? false: true}
+            onClick={handleSearch}>Search</button>
+          </div>
         </div>
       </div>
     </header>
   );
 };
 
+// {themeOptions.map((opt) => (
+//   <button
+//     key={opt.text}
+//     onClick={() => setTheme(opt.text)}
+//     className={`w-5 h-5 rounded-full text-xl leading-8 ${
+//       theme === opt.text && "text-accent dark:text-dark_accent"
+//     } transition ease-in duration-200`}
+//   >
+//     {opt.icon}
+//   </button>
+// ))}
 export default Navbar;
